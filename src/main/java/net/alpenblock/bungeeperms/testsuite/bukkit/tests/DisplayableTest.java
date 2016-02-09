6ae327e42d5548316e774224e061c97f21b3f26d
wea_ondara <wea_ondara@wea-ondara.net>
2016-02-09 12:20:17 +0100

    ported test to bukkit

---
+++ src/main/java/net/alpenblock/bungeeperms/testsuite/bukkit/tests/DisplayableTest.java
@@ -0,0 +1,39 @@
+package net.alpenblock.bungeeperms.testsuite.bukkit.tests;
+
+import net.alpenblock.bungeeperms.BungeePerms;
+import net.alpenblock.bungeeperms.User;
+import net.alpenblock.bungeeperms.testsuite.bukkit.BukkitTest;
+import net.alpenblock.bungeeperms.testsuite.bukkit.BukkitTestSuite;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+public class DisplayableTest extends BukkitTest
+{
+
+    @Override
+    public boolean test(CommandSender sender)
+    {
+        if (Bukkit.getPlayer(BukkitTestSuite.getTestplayer()) == null)
+        {
+            throw new RuntimeException("test player " + BukkitTestSuite.getTestplayer() + " not found");
+        }
+
+        User u = BungeePerms.getInstance().getPermissionsManager().getUser(BukkitTestSuite.getTestplayer());
+        if (u == null)
+        {
+            throw new RuntimeException("test player " + BukkitTestSuite.getTestplayer() + " not found");
+        }
+
+        sender.sendMessage("Prefix: " + u.buildPrefix().replaceAll("§", "&"));
+        sender.sendMessage("Suffix: " + u.buildSuffix().replaceAll("§", "&"));
+
+        return result();
+    }
+
+    @Override
+    public String getName()
+    {
+        return "DisplayableTest";
+    }
+
+}

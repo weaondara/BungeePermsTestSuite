2fd6cfabb8ec2d2bffec8be11e88a288763497d8
wea_ondara <wea_ondara@wea-ondara.net>
2016-02-09 13:14:20 +0100

    added vault displayable test

--- src/main/java/net/alpenblock/bungeeperms/testsuite/bukkit/BukkitTestSuite.java
+++ src/main/java/net/alpenblock/bungeeperms/testsuite/bukkit/BukkitTestSuite.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 import net.alpenblock.bungeeperms.ChatColor;
 import net.alpenblock.bungeeperms.Color;
 import net.alpenblock.bungeeperms.testsuite.bukkit.tests.AllowOpsTest;
+import net.alpenblock.bungeeperms.testsuite.bukkit.tests.DisplayableTest;
 import net.alpenblock.bungeeperms.testsuite.bukkit.tests.SuperPermsIterate;
 import net.alpenblock.bungeeperms.testsuite.bukkit.tests.SuperPermsList;
 import net.alpenblock.bungeeperms.testsuite.bukkit.tests.SuperPermsTest;
 import net.alpenblock.bungeeperms.testsuite.bukkit.tests.PrefixSuffixTest;
 import net.alpenblock.bungeeperms.testsuite.bukkit.tests.RecalculatePermissionsTest;
+import net.alpenblock.bungeeperms.testsuite.bukkit.tests.VaultDisplayableTest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
@@ -20,27 +22,30 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitTestSuite extends JavaPlugin
 {
+
     @Getter
     private static BukkitTestSuite instance;
-    
+
     @Getter
     private static String testplayer = "wea_ondara";
-    
+
     private final List<BukkitTest> tests = new ArrayList();
-    
+
     @Override
     public void onLoad()
     {
         instance = this;
-        
+
         tests.add(new AllowOpsTest());
+        tests.add(new DisplayableTest());
         tests.add(new PrefixSuffixTest());
         tests.add(new RecalculatePermissionsTest());
         tests.add(new SuperPermsIterate());
         tests.add(new SuperPermsTest());
         tests.add(new SuperPermsList());
+        tests.add(new VaultDisplayableTest());
     }
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
@@ -48,12 +53,12 @@ public class BukkitTestSuite extends JavaPlugin
         {
             return false;
         }
-        
+
         if (!(sender instanceof ConsoleCommandSender))
         {
             return false;
         }
-        
+
         if (args.length == 1 && args[0].equalsIgnoreCase("help"))
         {
             sender.sendMessage("/bpts help - Shows this help");
@@ -93,11 +98,11 @@ public class BukkitTestSuite extends JavaPlugin
             test(sender, tests);
             return true;
         }
-        
+
         sender.sendMessage(Color.Error + "Command not found.");
         return false;
     }
-    
+
     private void test(CommandSender sender, String... tests)
     {
         int error = 0;
@@ -132,7 +137,7 @@ public class BukkitTestSuite extends JavaPlugin
         }
         sender.sendMessage("Tests run: " + all + ", Failures: " + fail + ", Errors: " + error + ", Skipped: 0, Time elapsed: " + (double) time / 1000 + " sec");
     }
-    
+
     private BukkitTest getTest(String name)
     {
         for (BukkitTest t : tests)
---
+++ src/main/java/net/alpenblock/bungeeperms/testsuite/bukkit/tests/VaultDisplayableTest.java
@@ -0,0 +1,58 @@
+package net.alpenblock.bungeeperms.testsuite.bukkit.tests;
+
+import net.alpenblock.bungeeperms.BungeePerms;
+import net.alpenblock.bungeeperms.User;
+import net.alpenblock.bungeeperms.testsuite.bukkit.BukkitTest;
+import net.alpenblock.bungeeperms.testsuite.bukkit.BukkitTestSuite;
+import net.milkbowl.vault.chat.Chat;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class VaultDisplayableTest extends BukkitTest
+{
+
+    @Override
+    public boolean test(CommandSender sender)
+    {
+        if (Bukkit.getPlayer(BukkitTestSuite.getTestplayer()) == null)
+        {
+            throw new RuntimeException("test player " + BukkitTestSuite.getTestplayer() + " not found");
+        }
+
+        User u = BungeePerms.getInstance().getPermissionsManager().getUser(BukkitTestSuite.getTestplayer());
+        if (u == null)
+        {
+            throw new RuntimeException("test player " + BukkitTestSuite.getTestplayer() + " not found");
+        }
+
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null)
+        {
+            throw new RuntimeException("vault not present");
+        }
+        RegisteredServiceProvider<Chat> rsp = Bukkit.getServicesManager().getRegistration(Chat.class);
+        if (rsp == null)
+        {
+            throw new RuntimeException("vault: no chat service provider found");
+        }
+        Chat chat = rsp.getProvider();
+        if (!chat.getName().equals("BungeePerms"))
+        {
+            throw new RuntimeException("vault: chat service provider is not bungeeperms");
+        }
+
+        sender.sendMessage("BP    Prefix: " + u.buildPrefix().replaceAll("§", "&"));
+        sender.sendMessage("Vault Prefix: " + chat.getPlayerPrefix(Bukkit.getPlayer(BukkitTestSuite.getTestplayer())).replaceAll("§", "&"));
+        sender.sendMessage("BP    Suffix: " + u.buildSuffix().replaceAll("§", "&"));
+        sender.sendMessage("Vault Suffix: " + chat.getPlayerSuffix(Bukkit.getPlayer(BukkitTestSuite.getTestplayer())).replaceAll("§", "&"));
+
+        return result();
+    }
+
+    @Override
+    public String getName()
+    {
+        return "VaultDisplayableTest";
+    }
+
+}
